706a4a7ea571cad9e08eccbf407fbb40ac30291c
ShivajiRatnala <132065584+ShivajiRatnala@users.noreply.github.com>
2024-01-07 16:38:16 +0530

    Update settings.py

--- copyproject/copyproject/settings.py
+++ copyproject/copyproject/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'copyproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'copy',
-        'USER':'root',
-        'PASSWORD':'93904shiva',
-        'HOST':'localhost',
-        'PORT':3306,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
